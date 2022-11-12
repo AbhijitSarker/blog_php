0c1a8101d342eee2e7d0113b3aa8a0d88ec12cef
Abhijitsarker <abhijitsarker03@gmail.com>
2022-11-12 09:15:18 +0600

    manage category

--- admin/class/function.php
+++ admin/class/function.php
@@ -53,6 +53,89 @@
 
         }
 
-    }
+        public function display_category(){
+            $query = "SELECT * FROM category";
+            if(mysqli_query($this->conn, $query)){
+                $category = mysqli_query($this->conn, $query);
+                return $category;
+            }
+        }
 
-?>
+        public function delete_category($id){
+            $query = "DELETE FROM category WHERE cat_id=$id";
+
+            if(mysqli_query($this->conn, $query)){
+                return "Category Deleted Succesfully";
+            }
+        }
+
+        public function edit_category($id){
+            $cat_name = $_POST['cat_name'];
+	        $cat_des = $_POST['cat_des'];
+
+            $edit_option = $_GET['status'];
+
+            $edit_id = $_GET['id'];
+            
+            
+            if($edit_option=="edit")
+            { 
+                ?>
+                
+                <?php
+                $query_edit = "SELECT cat_name, cat_des FROM category WHERE cat_id=$edit_id";
+                $query_edit_ex = mysqli_query($this->conn, $query_edit);
+                $num_rows = mysqli_num_rows($query_edit_ex);
+                
+                $edit_query_fetch = mysqli_fetch_all($query_edit_ex, MYSQLI_ASSOC);
+
+                foreach($edit_query_fetch as $edit_query_item){
+                    $edit_cat_name = $edit_query_item['cat_name'];
+                    $edit_cat_des = $edit_query_item['cat_des'];
+                    print_r($edit_query_item) ;
+                }
+
+                ?>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label class="mb-1" for="cat_name">Edit Name</label>
+                        <input name="edit_cat_name" value="<?php echo $edit_cat_name; ?>" class="form-control py-4" id="cat_name" type="text"/>
+                    </div>
+                    <div class="form-group">
+                        <label class="mb-1" for="cat_des">Edit Description</label>
+                        <input name="edit_cat_des" value="<?php echo $edit_cat_des; ?>" class="form-control py-4" id="cat_des" type="text"/>
+                    </div>
+                    <input name="update_cat_btn" class="btn btn-success" type="submit" value="Update Category">
+                </form>
+                
+            <?php 
+                
+        
+
+            if(isset($_POST['update_cat_btn'])){
+                $edited_cat_name = $_POST['edit_cat_name'];
+                $edited_cat_des = $_POST['edit_cat_des'];
+                $query =  "UPDATE category SET cat_name='$edited_cat_name', cat_des='$edited_cat_des' WHERE cat_id=$id";
+                mysqli_query($this->conn, $query);
+                
+                ?> 
+                <script>
+
+                    location.href = "http://localhost/blog/admin/manage_category.php";
+
+                </script>
+                <?php
+            }
+
+
+
+
+            // if(mysqli_query($this->conn, $query)){
+
+            //     return "Category Edited Succesfully";
+            // }
+        }
+
+    }
+}
+
--- admin/view/manage_cat_view.php
+++ admin/view/manage_cat_view.php
@@ -1,1 +1,89 @@
-<h1>Manage Category</h1>
+<?php
+    $catdata = $obj->display_category();
+
+    if(isset($_GET['status'])){
+        if($_GET['status']=='delete'){
+            $delid = $_GET['id'];
+            $msg = $obj->delete_category($delid);
+            echo $msg; 
+            
+            ?>
+
+            <script>
+
+            location.href = "http://localhost/blog/admin/manage_category.php";
+
+            var urlParams = new URLSearchParams(window.location.search);
+
+
+
+
+if(urlParams.get('status')  == 'delete'){
+    alert("deleted successfully");
+}
+
+
+
+            </script>
+
+
+     <?php   }
+     
+        if($_GET['status']=='edit'){
+            $editid = $_GET['id'];
+            $msg= $obj->edit_category($editid);
+            echo $msg;
+        }
+    }
+
+
+    if(isset($_POST['edit_btn'])){
+        $msg =$obj->edit_data($_POST);
+        echo $msg;
+
+    }
+
+?>
+
+
+<div class="manage_cat_items">
+    <h1>Manage Category</h1>
+
+    <?php if(isset($msg)){
+        echo $msg;
+    } ?>
+
+    <table class="table">
+        <thead>
+            <th>ID</th>
+            <th>Category name</th>
+            <th>Category Description</th>
+            <th>Action</th>
+        </thead>
+        <tbody>
+            <?php while($cat = mysqli_fetch_assoc($catdata)){ ?>
+            <tr>
+                <td><?php echo $cat['cat_id'];?></td>
+                <td><?php echo $cat['cat_name'];?></td>
+                <td><?php echo $cat['cat_des'];?></td>
+                <td>
+                    <a class="btn btn-success" href="?status=edit&&id=<?php echo $cat['cat_id'];?>">Edit</a>
+                    <a class="btn btn-warning" href="?status=delete&&id=<?php echo $cat['cat_id'];?>">Delete</a>
+                </td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<?php 
+     if(isset($_GET['status'])){
+     
+    
+?>
+<script>
+    document.querySelector(".manage_cat_items").style.display = 'none';                    
+</script>
+<?php 
+   }
+?>
