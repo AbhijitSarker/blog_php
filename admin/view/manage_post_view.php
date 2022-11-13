16e1829b062e2c46d63ea70681af11540aac41c5
Abhijitsarker <abhijitsarker03@gmail.com>
2022-11-14 01:44:45 +0600

    manage post table

--- admin/view/manage_post_view.php
+++ admin/view/manage_post_view.php
@@ -1,1 +1,31 @@
 <h1>Manage post </h1>
+
+<table class="table table-hover table-dark">
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Content</th>
+            <th>Image</th>
+            <th>Category</th>
+            <th>Summery</th>
+            <th>Tags</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>post</td>
+            <td>saf</td>
+            <td>daf</td>
+            <td>asf</td>
+            <td>adsf</td>
+            <td>fdf</td>
+            <td>fg</td>
+            <td>sfg</td>
+            <td>fs</td>
+        </tr>
+    </tbody>
+</table>
